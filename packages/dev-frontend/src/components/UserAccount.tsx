9874bfced208d92f4b600d7d2425fb307e15f96d
evandrosaturnino <evandro.saturnino@gmail.com>
2024-08-21 12:07:50 -0300

    feat: enable auto connect to gnosis safe app

--- packages/dev-frontend/src/components/UserAccount.tsx
+++ packages/dev-frontend/src/components/UserAccount.tsx
@@ -1,40 +1,52 @@
-import { Text, Flex, Box } from "theme-ui";
-
-import { shortenAddress } from "../utils/shortenAddress";
-import { useConnectWallet } from "@web3-onboard/react";
-import { WalletState } from "@web3-onboard/core";
+import { Text, Flex, Box } from "theme-ui"
+import { shortenAddress } from "../utils/shortenAddress"
+import { useConnectWallet } from "@web3-onboard/react"
+import { WalletState } from "@web3-onboard/core"
+import { useEffect } from "react"
 
 export const UserAccount = (): JSX.Element => {
-  const [{ wallet }, connect, disconnect] = useConnectWallet();
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
 
   const disconnectWallet = (wallet: WalletState) => {
     localStorage.removeItem('walletLabel')
     disconnect(wallet)
   }
 
+  useEffect(() => {
+    const loadedInIframe = window.self !== window.top
+    if (loadedInIframe && !wallet && !connecting) {
+      connect({
+        autoSelect: {
+          label: 'Safe',
+          disableModals: true
+        }
+      })
+    }
+  }, [wallet, connecting, connect])
+
   if (wallet?.accounts[0]) {
     return (
       <Box
         sx={{ cursor: "pointer" }}
         onClick={() => {
-          disconnectWallet(wallet);
+          disconnectWallet(wallet)
         }}
       >
         <Flex variant="layout.userAccount">
           <Flex variant="layout.account">
             <Text as="span" sx={{ fontSize: "0.8rem", fontWeight: "bold" }}>
-              {wallet?.accounts[0].address ?  shortenAddress(wallet?.accounts[0].address) : 'Connect Wallet'}
+              {wallet?.accounts[0].address ? shortenAddress(wallet?.accounts[0].address) : 'Connect Wallet'}
             </Text>
           </Flex>
         </Flex>
       </Box>
-    );
+    )
   } else {
     return (
       <Box
         sx={{ cursor: "pointer" }}
         onClick={() => {
-          connect();
+          connect()
         }}
       >
         <Flex variant="layout.userAccount">
@@ -45,5 +57,6 @@ export const UserAccount = (): JSX.Element => {
           </Flex>
         </Flex>
       </Box>
-    )}
-};
+    )
+  }
+}
